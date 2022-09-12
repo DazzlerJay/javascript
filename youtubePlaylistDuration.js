07a85d0cf90627e7fcb88c0b55f7c32a03b12ace
Jay Joshi <39905401+DazzlerJay@users.noreply.github.com>
2022-09-13 01:56:03 +0530

    Add files via upload
    Added: 1. Created a script for fetching time duration of a youtube playlist
    TODO: 1. Convert time into a human readable format
    Usage: 1. Paste the script in console under the youtube playlist tab

---
+++ youtubePlaylistDuration.js
@@ -0,0 +1,41 @@
+/**
+ * returns time duration of playlist in hours, minutes and seconds format
+ * */
+
+const getPlaylistDuration = () => {
+  document
+    .querySelectorAll("ytd-playlist-panel-video-renderer")[0]
+    .querySelectorAll(".ytd-thumbnail-overlay-time-status-renderer")[1]
+    .ariaLabel;
+
+  const time = {
+    hour: 0,
+    minute: 0,
+    second: 0,
+  };
+
+  // singular as it covers both string cases: 0. hour 1. hours
+  const HOUR = "hour";
+  const MINUTE = "minute";
+  const SECOND = "second";
+
+  const videoComponentTag = "ytd-playlist-panel-video-renderer";
+  const videoTimeLabelClass = ".ytd-thumbnail-overlay-time-status-renderer";
+
+  document.querySelectorAll(videoComponentTag).forEach((video) => {
+    const timeLabel = video.querySelectorAll(videoTimeLabelClass)[1].ariaLabel;
+
+    timeLabel.split(",").forEach((metric) => {
+      const [value, label] = metric.trim().split(" ");
+      if (label.includes(HOUR)) time[HOUR] += parseInt(value);
+      else if (label.includes(MINUTE)) time[MINUTE] += parseInt(value);
+      else if (label.includes(SECOND)) time[SECOND] += parseInt(value);
+    });
+  });
+
+  // TODO: Convert into standard time format
+  // Eg: hours: 0, minutes: 75, seconds: 65 to hours: 1 , minutes: 16, seconds: 5
+  console.log(time);
+};
+
+getPlaylistDuration();
